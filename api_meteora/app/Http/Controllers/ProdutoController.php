c0aba52aba1f615b938fdad8f550859f5463996a
Seu Nome <seu_email@example.com>
2024-11-12 21:25:34 -0300

    aula_19

--- api_meteora/app/Http/Controllers/ProdutoController.php
+++ api_meteora/app/Http/Controllers/ProdutoController.php
@@ -6,5 +6,23 @@ use Illuminate\Http\Request;
 
 class ProdutoController extends Controller
 {
-    //
+    public function  create() {
+        return view('produtos.create');;
+    }
+
+    public function store(Request $request) {
+        $request->validate([
+            'nome'=> 'required|string|max:200',
+            'descricao'=>'nullable|text',
+            'tamanho'=> 'nulable|string|max:10',
+            'cor'=>'nulable|string|max:50',
+            'preco'=>'required|decimal|min:0',
+            'quantidade'=>'required|integer|min:0',
+            'imagem'=>'nullable|image|mimes:jpeg,png,jpg,gif|max:2048'
+        ]);
+
+        $imagem = $request->fil('imagem');
+
+        $caminhoImagem = $imagem ? $imagem->store('imagens/produtos','public') : null;
+    }
 }
--- api_meteora/app/Models/Produto.php
+++ api_meteora/app/Models/Produto.php
@@ -9,18 +9,6 @@ class Produto extends Model
 {
     use HasFactory;
 
-    Schema::create('produtos', function(Blueprint $table) {
-        $table->id();
-        $table->string('nome');
-        $table->text('descricao')->nullable();
-        $table->string('tamanho')->nullable();
-        $table->string('cor')->nullable();
-        $table->decimal('preco', 8, 2);
-        $table->integer('quantidade');
-        $table->string('imagem')->nullable();
-        $table->timestamps();
-    });
-    
     protected $fillable = [
         'nome',
         'descricao',
